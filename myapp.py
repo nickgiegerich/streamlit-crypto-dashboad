33cda0cbd1d4397e70583c675a38ea065ee902d2
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-11-24 00:05:19 -0800

    beginning dynamic candle stick charts

--- myapp.py
+++ myapp.py
@@ -5,6 +5,7 @@ import numpy as np
 import cbpro
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
+import time
 
 public_client = cbpro.PublicClient()
 
@@ -103,7 +104,10 @@ the rows are ordered as follows:
 
 """, coins, df_for_product_ticker)
 
-st.date_input('enter a date')
+
+
+from_date = st.sidebar.date_input('from date')
+to_date = st.sidebar.date_input('to date')
 
 default_date = dt.date.today()
 default_beg_date = default_date - dt.timedelta(days=30)
@@ -111,14 +115,12 @@ default_beg_date = default_date - dt.timedelta(days=30)
 date_list = []
 date_list.append(default_date)
 
-increment_date = default_date
-while increment_date != default_beg_date:
+increment_date = to_date
+while increment_date != from_date:
     increment_date -= dt.timedelta(days=1)
     date_list.append(increment_date)
 
-print(date_list)
-
-historic_data = public_client.get_product_historic_rates('ETH-USD', default_beg_date, default_date, 86400)
+historic_data = public_client.get_product_historic_rates('ETH-USD', from_date, to_date, 86400)
 df_for_historic_data = pd.DataFrame(historic_data)
 
 fig = go.FigureWidget(data=[go.Candlestick(x=date_list,
@@ -129,9 +131,14 @@ fig = go.FigureWidget(data=[go.Candlestick(x=date_list,
 
 fig.update_layout(
     title='ETH-USD stock price',
-    yaxis_title='ETH-USD price'
+    yaxis_title='ETH-USD price',
+    xaxis_title='Date'
 )
 
-st.write(historic_data)
-st.plotly_chart(fig)
+figure_one = st.plotly_chart(fig, use_container_width=True)
 # st.write(df_for_historic_data)
+
+col1, col2 = st.beta_columns(2)
+
+col1.plotly_chart(fig, use_container_width=True)
+col2.plotly_chart(fig, use_container_width=True)
